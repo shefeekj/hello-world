4f80438c3e4b71cf1d288257f106f756e1f2e6d8
shefeekj <55088049+shefeekj@users.noreply.github.com>
2024-11-11 10:22:40 +0530

    Update Main.java

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,17 @@
 package org.example;
 
 public class Main {
+
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        
+        // Call sayHello method
+        sayHello("Shefeek");
+    }
+
+    // New sayHello method
+    public static void sayHello(String name) {
+        System.out.println("Hello, " + name + "!");
     }
 }
+
